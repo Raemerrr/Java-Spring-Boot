28d7fa3587b5c08a7e2a34ce602cb6892fd535b9
Raemin KANG <foals18@gmail.com>
2020-06-14 14:10:58 +0900

    7~8영상 업로드

---
+++ Spring/FirstProject/src/main/java/com/javalec/ex/AdminConnection.java
@@ -0,0 +1,45 @@
+package com.javalec.ex;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class AdminConnection implements InitializingBean, DisposableBean {
+	private String adminID;
+	private String adminPW;
+	
+	AdminConnection(String adminID, String adminPW)
+	{
+		this.adminID = adminID;
+		this.adminPW = adminPW;
+	}
+
+	public String getAdminID() {
+		return adminID;
+	}
+
+	public void setAdminID(String adminID) {
+		this.adminID = adminID;
+	}
+
+	public String getAdminPW() {
+		return adminPW;
+	}
+
+	public void setAdminPW(String adminPW) {
+		this.adminPW = adminPW;
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("destroy()!!!");
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("afterPropertiesSet()!!");
+	}
+	
+	
+}
--- Spring/FirstProject/src/main/java/com/javalec/ex/MainClass.java
+++ Spring/FirstProject/src/main/java/com/javalec/ex/MainClass.java
@@ -8,9 +8,9 @@ public class MainClass {
 	public static void main(String[] args) {
 		String _path = "classpath:applicationCTX.xml";
 		AbstractApplicationContext ctx = new GenericXmlApplicationContext(_path);
-		Pencil p = ctx.getBean("pencil",Pencil.class);
-		p.use();
-		//사용이 끝나면 닫아주자!
+		AdminConnection connection = ctx.getBean("adminConnection",AdminConnection.class);
+		System.out.println(connection.getAdminID());
+		System.out.println(connection.getAdminPW());
 		ctx.close();
 	}
 }
