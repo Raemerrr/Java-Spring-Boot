ba8c0dbc4e390800916d0767dce524f13762ae74
Raemin KANG <foals18@gmail.com>
2021-08-19 08:30:06 +0900

    변경점 업로드

---
+++ Java/java8to11/src/main/java/me/raemerrr/java8to11/ch10/App.java
@@ -0,0 +1,27 @@
+package me.raemerrr.java8to11.ch10;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class App {
+    public static void main(String[] args) {
+        List<OnlineClass> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(5, "rest api development", false));
+
+        Optional<OnlineClass> optional = springClasses.stream()
+                .filter(oc -> oc.getTitle().startsWith("spring"))
+                .findFirst();
+    }
+
+    private static OnlineClass createNewClasss() {
+        System.out.println("creating new online class");
+        return new OnlineClass(10, "New class", false);
+    }
+}
