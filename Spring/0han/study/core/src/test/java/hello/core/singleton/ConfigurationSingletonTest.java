4c7b9dc89a70c653b65cdc01a9e782b53f3a4c2a
Raemin KANG <foals18@gmail.com>
2021-08-27 16:20:09 +0900

    [테스트 코드] - @Configuration과 싱글톤

---
+++ Spring/0han/study/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,36 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    @DisplayName("스프링 빈 객체 비교")
+    void configurationSingletonTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+        System.out.println("memberService -> memberRepository : " + memberRepository1);
+        System.out.println("orderService -> memberRepository : " + memberRepository2);
+        System.out.println("memberRepository : " + memberRepository);
+
+        Assertions.assertThat(memberRepository1).isSameAs(memberRepository2).isSameAs(memberRepository);
+    }
+}
