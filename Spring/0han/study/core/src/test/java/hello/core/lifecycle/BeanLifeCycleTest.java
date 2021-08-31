1cda02ad2ce6ff588a61d5f47af920d429d3b312
Raemin KANG <foals18@gmail.com>
2021-08-31 22:11:45 +0900

    [테스트 코드] - 빈 등록 초기화, 소멸 메서드

--- Spring/0han/study/core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ Spring/0han/study/core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -18,7 +18,7 @@ public class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig {
-        @Bean
+        @Bean(initMethod = "init", destroyMethod = "close")
         public NetworkClient networkClient() {
             NetworkClient client = new NetworkClient();
             client.setUrl("http://hello-spring.dev");
