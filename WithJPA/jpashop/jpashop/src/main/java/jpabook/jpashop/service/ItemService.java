7393dd8daf40fafc9de1731a3b161e416f1f6b1e
Raemin KANG <foals18@gmail.com>
2021-09-28 07:11:07 +0900

    [상품 도메인 개발] - 상품 서비스 개발

---
+++ WithJPA/jpashop/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        //상품은 동일 이름으로 등록되어도 되는가?
+        //validateDuplicateItem(item);
+        itemRepository.save(item);
+    }
+
+    private void validateDuplicateItem(Item item) {
+        List<Item> findList = itemRepository.findByName(item.getName());
+        if (!findList.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 아이템입니다.");
+        }
+    }
+
+    //아이템 전체 조회
+    public List<Item> findAll() {
+        return itemRepository.findAll();
+    }
+
+    //아이템 단건 조회
+    public Item findOne(Long id) {
+        return itemRepository.findOne(id);
+    }
+}
