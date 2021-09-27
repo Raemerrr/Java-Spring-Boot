dc41d5b7de54cb019de9ece53707af88361a0a38
Raemin KANG <foals18@gmail.com>
2021-09-27 17:27:25 +0900

    [도메인 분석 설계] - 엔티티 설계시 주의점

--- WithJPA/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ WithJPA/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -18,14 +18,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -33,4 +33,32 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 [ORDER, CANCEL]
+
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    //==연관관계 메서드 예시==//
+//    public static void main(String[] args) {
+//        Member member = new Member();
+//        Order order = new Order();
+//
+//        //아래의 두 곳에 모두 연관관계를 맺어줘야하는데..
+//        //휴먼에러가 발생할 확률이 높다...
+//        member.getOrders().add(order);
+//        order.setMember(member);
+//    }
+
+    //==연관관계 메서드==//
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    //==연관관계 메서드==//
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
