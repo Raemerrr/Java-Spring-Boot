2c4fd436eb3cdb9fc38bf7c0fbfcd837569143c4
raemerrr <foals18@gmail.com>
2021-11-16 09:20:29 +0900

    feat: 멀티스레드 - 콜백 처리 업로드 * CallbackExample.java

---
+++ Java/java8to11/src/main/java/me/raemerrr/java/multithread/callback/CallbackExample.java
@@ -0,0 +1,65 @@
+package me.raemerrr.java.multithread.callback;
+
+import java.nio.channels.CompletionHandler;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CallbackExample {
+
+    private ExecutorService executorService;
+
+    public CallbackExample() {
+        executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    }
+
+    private CompletionHandler<Integer, Void> callback = new CompletionHandler<>() {
+        @Override
+        public void completed(Integer result, Void attachment) {
+            System.out.println("completed() 실행 : " + result);
+        }
+
+        @Override
+        public void failed(Throwable exc, Void attachment) {
+            System.out.println("failed() 실행 : " + exc.toString());
+        }
+    };
+
+    public void doWork(final String x, final String y) {
+//        Runnable task = new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    int intX = Integer.parseInt(x);
+//                    int intY = Integer.parseInt(y);
+//                    int result = intX + intY;
+//                    callback.completed(result, null); // 정상 처리
+//                } catch (NumberFormatException e) {
+//                    callback.failed(e, null); // 실패 처리
+//                }
+//            }
+//        };
+        Runnable task = () -> {
+            try {
+                int intX = Integer.parseInt(x);
+                int intY = Integer.parseInt(y);
+                int result = intX + intY;
+                callback.completed(result, null); // 정상 처리
+            } catch (NumberFormatException e) {
+                callback.failed(e, null); // 실패 처리
+            }
+        };
+        executorService.submit(task);
+    }
+
+    public void finish() {
+        executorService.shutdown();
+    }
+
+    public static void main(String[] args) {
+        CallbackExample example = new CallbackExample();
+        example.doWork("3", "3");
+        example.doWork("3", "삼");
+        example.finish();
+    }
+
+}
